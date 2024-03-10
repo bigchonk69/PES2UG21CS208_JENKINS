c1af074051f41bb11e3cc7f3380c5c7485e5b884
itish shukla <132115503+bigchonk69@users.noreply.github.com>
2024-03-10 15:56:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "webhook succesfully created";
+  cout << "webhook succesfully created"\n;
   return 0;
 }
